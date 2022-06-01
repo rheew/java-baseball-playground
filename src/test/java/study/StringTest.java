f3e0b299980817dbe3ccdd84109951f762117c6e
rheew <47098896+rheew@users.noreply.github.com>
2022-06-01 15:39:46 +0900

    test: split 학습테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,14 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void split() {
+        String[] result1 = "1,2".split(",");
+        String[] result2 = "1".split(",");
+
+        assertThat(result1).contains("1","2");
+        assertThat(result2).containsExactly("1");
+    }
+
 }
