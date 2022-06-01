8288ef8fff88bfe6c83f09a17a3e9fd3a98f27a4
rheew <47098896+rheew@users.noreply.github.com>
2022-06-01 15:54:25 +0900

    test: charAt exception 학습테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class StringTest {
     @Test
@@ -26,4 +28,19 @@ public class StringTest {
         assertThat(result).isEqualTo("1,2");
     }
 
+    @Test
+    void findCharAt() {
+        char result = "abc".charAt(1);
+        assertThat(result).isEqualTo('b');
+    }
+
+    @DisplayName("위차 값을 넘어가면 StringIndexBoundsException이 발생한다")
+    @Test
+    void findCharAtException() {
+
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    "abc".charAt(4);
+                }).withMessageMatching("String index out of range: 4");
+    }
 }
