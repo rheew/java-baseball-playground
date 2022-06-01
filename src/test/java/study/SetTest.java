39d2ba8871a6b7e7d38e905b318bcc6a7c8e3f0f
rheew <47098896+rheew@users.noreply.github.com>
2022-06-01 16:23:10 +0900

    test: set 학습 테스트 - contains false, true 테스트 추가

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -2,14 +2,17 @@ package study;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SetTest {
@@ -36,5 +39,13 @@ public class SetTest {
     void contains(int param) {
 
         assertTrue(numbers.contains(param));
+    }
+
+    @DisplayName("contains에서 값을 찾는 경우와 못찾는 경우를 테스트 한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void containsTrueAndFalse(int num, boolean result) {
+
+        assertEquals(numbers.contains(num), result);
     }
 }
